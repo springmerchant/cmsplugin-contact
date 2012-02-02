18ad9954f4b68bac148dccf23e9a85df2a1db127
Emilian Felecan <emil@springmerchant>
2012-02-02 06:49:33 +0200

    Adding the BC related fields

--- cmsplugin_contact/forms.py
+++ cmsplugin_contact/forms.py
@@ -1,11 +1,31 @@
 from django import forms
 #import settings
 from cmsplugin_contact.nospam.forms import HoneyPotForm, RecaptchaForm, AkismetForm
+
+SERVICES = (
+    ('bcdesign', 'BigCommerce Templates'),
+    ('bcmod', 'BigCommerce Modifications (Jquery)'),
+    ('bcauto', 'BigCommerce Automation'),
+    ('bctraining', 'BigCommerce Training'),
+    ('bcseo', 'BigCommerce SEO'),
+    )
+
+BUDGET = (
+    ('under500', '< $500'),
+    ('over1000', '$1000 - $3000'),
+    ('over3000', '$3000 - $5000'),
+    ('over5000', '$5000 - $7000'),
+    ('other', 'Other'),
+    )
   
 class ContactForm(forms.Form):
     email = forms.EmailField()
     subject = forms.CharField(required=False)
     content = forms.CharField(widget=forms.Textarea())
+    website_url = forms.CharField(label=u'Website URL', initial='http://', required=False, widget=TextInput(attrs={'class': 'form-field rounded-field'}))
+    services = forms.MultipleChoiceField(required=False, widget=CheckboxSelectMultiple, choices=SERVICES)
+    phone = forms.CharField(required=True, widget=TextInput(attrs={'class': 'form-field rounded-field'}))
+    budget = forms.ChoiceField(required=True, choices=BUDGET)
 
   
 class HoneyPotContactForm(HoneyPotForm):
